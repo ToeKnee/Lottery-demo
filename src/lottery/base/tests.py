6ed0b8de44f874319bb70fbf51c9978c8194eb85
Anthony Batchelor <tony@ynottony.net>
2014-10-06 16:12:21 +0100

    Handle Anonymous users on home page

--- src/lottery/base/tests.py
+++ src/lottery/base/tests.py
@@ -1,3 +1,72 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.test import (
+    TestCase,
+    RequestFactory,
+)
 
-# Create your tests here.
+
+from .factories import UserFactory
+from .test_utils import strip_spaces_from_html
+from .views import home
+from lottery.lotto.factories import ActiveLotteryFactory
+
+
+class HomeView(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_anonymous__no_lotteries(self):
+        request = self.factory.get('')
+        request.user = AnonymousUser()
+
+        response = home(request)
+        self.assertEqual(200, response.status_code)
+        content = strip_spaces_from_html(response.content)
+        self.assertIn("No lotteries active", content)
+
+    def test_anonymous__with_lottery(self):
+        lottery = ActiveLotteryFactory()
+        request = self.factory.get('')
+        request.user = AnonymousUser()
+
+        response = home(request)
+        self.assertEqual(200, response.status_code)
+        content = strip_spaces_from_html(response.content)
+        self.assertIn(lottery.title, content)
+        self.assertIn(lottery.get_absolute_url(), content)
+
+    def test_no_lotteries(self):
+        user = UserFactory()
+        request = self.factory.get('')
+        request.user = user
+
+        response = home(request)
+        self.assertEqual(200, response.status_code)
+        content = strip_spaces_from_html(response.content)
+        self.assertIn("No lotteries active", content)
+
+    def test_with_lottery__not_entered(self):
+        lottery = ActiveLotteryFactory()
+        user = UserFactory()
+        request = self.factory.get('')
+        request.user = user
+
+        response = home(request)
+        self.assertEqual(200, response.status_code)
+        content = strip_spaces_from_html(response.content)
+        self.assertIn(lottery.title, content)
+        self.assertIn(lottery.get_absolute_url(), content)
+
+    def test_with_lottery_entered(self):
+        lottery = ActiveLotteryFactory()
+        user = UserFactory()
+        lottery.entrants.add(user)
+
+        request = self.factory.get('')
+        request.user = user
+
+        response = home(request)
+        self.assertEqual(200, response.status_code)
+        content = strip_spaces_from_html(response.content)
+        self.assertIn(lottery.title, content)
+        self.assertNotIn(lottery.get_absolute_url(), content)
--- src/lottery/base/views.py
+++ src/lottery/base/views.py
@@ -3,8 +3,12 @@ from lottery.lotto.models import Lottery
 
 
 def home(request):
-    lotteries_entered = Lottery.objects.active().filter(entrants=request.user)
-    lotteries_open = Lottery.objects.active().exclude(entrants=request.user)
+    if request.user.is_anonymous():
+        lotteries_entered = []
+        lotteries_open = Lottery.objects.active()
+    else:
+        lotteries_entered = Lottery.objects.active().filter(entrants=request.user)
+        lotteries_open = Lottery.objects.active().exclude(entrants=request.user)
     context = {
         "lotteries_entered": lotteries_entered,
         "lotteries_open": lotteries_open,
